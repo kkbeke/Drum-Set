24a4803916a874f64c33b7773f7a57c055f79299
Carlos Echavez <carlosechavezjr@gmail.com>
2023-07-24 22:27:46 +0800

    audio

--- index.js
+++ index.js
@@ -1,41 +1,45 @@
 
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
-var crash = new Audio('sounds/crash.mp3');
-var kickBass = new Audio('sounds/kick-bass.mp3');
-var snare = new Audio('sounds/snare.mp3');
-var tom1 = new Audio('sounds/tom-1.mp3');
-var tom2 = new Audio('sounds/tom-2.mp3');
-var tom3 = new Audio('sounds/tom-3.mp3');
-var tom4 = new Audio('sounds/tom-4.mp3');
-
-function playCrash() {
-    crash.play();
-}
-function playkick() {
-    kickBass.play();
-}
-function playsnare() {
-    snare.play();
-}
-function playtom1() {
-    tom1.play();
-}
-function playtom2() {
-    tom2.play();
-}
-function playtom3() {
-    tom3.play();
-}
-function playtom4() {
-    tom4.play();
-}
-
 for (var i =0; i < numberOfDrumButtons ; i++){
 
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
        
-        this.style.color = "white";
+        var buttonInnerHTML = this.innerHTML;
+
+        switch(buttonInnerHTML) {
+            case "w":
+                var tom1 = new Audio('sounds/tom-1.mp3');
+                tom1.play();
+                break;
+            case "a":
+                var tom2 = new Audio('sounds/tom-2.mp3');
+                tom2.play();
+                break;
+            case "s":
+                var tom3 = new Audio('sounds/tom-3.mp3');
+                tom3.play();
+                break;
+            case "d":
+                var tom4 = new Audio('sounds/tom-4.mp3');
+                tom4.play();
+                break;
+             case "j":
+                var crash = new Audio('sounds/crash.mp3');
+                crash.play();
+                break;
+            case "k":
+                var kick = new Audio('sounds/kick-bass.mp3');
+                kick.play();
+                break;
+            case "l":
+                var snare = new Audio('sounds/snare.mp3');
+                snare.play();
+                break;
+            
+            default:
+                console.log(buttonInnerHTML);
+        }
         
     });
 }
